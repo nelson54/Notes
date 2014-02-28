98969ee7c8b340a1939c516211ebe1730dbb0d4e
Derek Nelson <nelson54@ferris.edu>
2014-02-28 12:08:40 -0500

    Add checkboxe support [] and [x] are checkboxes

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,9 +15,13 @@ module.exports = function(grunt) {
             template: 'build/template/index.jst',
             markdownOptions: {
               gfm: true,
+              breaks: true,
+              highlight: function (code) {
+                  return require('highlight.js').highlightAuto(code).value;
+              },
               codeLines: {
-/*                before: '<span>',
-                after: '</span>'*/
+                before: '<span>',
+                after: '</span>'
               }
             }
           }
@@ -28,27 +32,43 @@ module.exports = function(grunt) {
           options: {
             port: 9001,
             hostname: '*',
-            base: 'build/public/',
-            livereload: 35729
+            base: 'build/public/'
           }
         }
       },
       watch: {
         scripts: {
-          files: 'notes/*.md',
+          files: 'notes*//*.md',
           tasks: ['markdown'],
           options: {
             debounceDelay: 250,
             livereload: true
           }
         }
+      },
+      markdownpdf: {
+        options: {
+            cssPath : "build/public/css/build.css"
+        },
+        files:{
+              src: 'notes/*.md',
+              dest: 'build/public/notes'
+        }
+      },
+      concat: {
+        dist : {
+            src:['build/public/css/normalize.css', 'build/public/css/bootstrap.css', 'build/public/css/main.css', 'build/public/css/github-highlight.css'],
+            dest: 'build/public/css/build.css'
+        }
       }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-markdown');
+    grunt.loadNpmTasks('grunt-markdown-pdf');
     grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('default', ['markdown', 'connect', 'watch']);
+    grunt.registerTask('default', ['concat', 'markdown', 'markdownpdf', 'connect', 'watch']);
 
 };
--- build/public/js/main.js
+++ build/public/js/main.js
@@ -1,1 +1,12 @@
+$(function(){
+    var pdfUrl = window.location.pathname.split("").reverse().join("").split('.', 2).pop().split("").reverse().concat(".pdf").join("");
+    var pdfLink = '<a href="'+pdfUrl+'">View as PDF</a>';
 
+    $(".pdf-link").append(pdfLink);
+
+    $('p').each(function(i, el){
+        el.innerHTML = $(el).html().replace(/\[\s]/g, "<input type='checkbox' />").replace(/\[x]/g, "<input type='checkbox' checked />");
+    });
+
+
+});
